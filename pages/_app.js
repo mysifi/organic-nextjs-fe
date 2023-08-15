37e35412d6f4ae2158d80457330eecc5ea5abcf7
mysifi <drhasifi@gmail.com>
2023-08-16 02:31:31 +0800

    tambahkan component, header, store, persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,39 @@
-import '@/styles/globals.scss'
-import Home from '.'
+import '@/styles/globals.scss';
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+let persistor = persistStore(store);
+// utk cart
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>Welcome | TazZA</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="css/icofont.min.css" />
+        <link rel="stylesheet" href="css/slick.css" />
+        <link rel="stylesheet" href="css/slick-theme.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/responsive.css" />
+        </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </div>
+  );
 }
+
+
+export default MyApp;
--- store/index.js
+++ store/index.js
@@ -1,1 +1,25 @@
-import { configureStore } from "reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+
+// reducer adalah utk simpan data dalam store, simpan dalam cart
+const reducers = combineReducers({ });
+
+// config utk reducer
+const config = {
+  key: "root",
+  storage,
+};
+
+const reducer = persistReducer(config, reducers);
+
+const store = configureStore({
+  reducer: reducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export default store;
